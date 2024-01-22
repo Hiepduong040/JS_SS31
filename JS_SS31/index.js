b0d7b0f2a3407a7be3e2b9bce4d28bb73db665a8
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2024-01-22 07:53:24 +0700

    Add files via upload

---
+++ JS_SS31/index.js
@@ -0,0 +1,13 @@
+// function calculate(a,b){
+//     return a + b;
+
+// }
+// calculate(10,20);
+// calculate(10,30);
+// calculate(10,40);
+// calculate(10,50);
+// calculate(-10,20);
+
+function increaseBy2(arr){
+    
+}
